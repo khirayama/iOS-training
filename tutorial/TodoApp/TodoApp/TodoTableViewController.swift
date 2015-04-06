498b31d43f3304685c297ec50d8a80cabcf2c056
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-04-07 07:12:02 +0900

    公式のtutorial（和訳）が動かない。だと！？

--- tutorial/TodoApp/TodoApp/TodoTableViewController.swift
+++ tutorial/TodoApp/TodoApp/TodoTableViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class TodoTableViewController : UIViewController {
-    override func viewDidLoad() { // 読み込まれた時に実行的な？
-        super.viewDidLoad();
+class TodoTableViewController : UIViewController, UITableViewDataSource {
+    var tableView : UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let header = UIImageView(frame: CGRect(x: 0, y: 0, width: 320, height: 64))
+        header.image = UIImage(named:"header")
         
         let title = UILabel(frame: CGRect(x: 10, y: 20, width: 310, height: 44))
-        title.text = "TODOリスト"
-        self.view.addSubview(title)
+        title.text = "Todoリスト"
+        header.addSubview(title)
+        
+        let screenWidth = UIScreen.mainScreen().bounds.size.height
+        self.tableView = UITableView(frame: CGRect(x: 0, y: 60, width: 320, height: screenWidth - 60))
+        self.tableView!.dataSource = self
+        
+        self.view.addSubview(self.tableView!)
+        self.view.addSubview(header)
+    }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: nil)
+        cell.textLabel.text = "todo"
+        return cell
     }
 }
