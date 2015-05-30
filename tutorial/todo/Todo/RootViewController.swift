c9b454394aa2a8a4fc206f920078151d4acddbc3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-05-30 09:26:29 +0900

    サンプル追加

---
+++ tutorial/todo/Todo/RootViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RootViewController.swift
+//  Todo
+//
+//  Created by Sugimoto.Hiroki on 2015/05/29.
+//  Copyright (c) 2015年 Sugimoto.Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITableViewController {
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        switch indexPath.row {
+        case 0: self.performSegueWithIdentifier("todo", sender: indexPath)
+        case 1: self.performSegueWithIdentifier("webView", sender: indexPath)
+        case 2: self.performSegueWithIdentifier("network", sender: indexPath)
+        default: break
+        }
+        
+//        self.navigationController?.pushViewController(TodoViewController(), animated: true)
+//        self.presentViewController(TodoViewController(), animated: true, completion: {})
+    }
+}
