32b7d6915e4c81875e58eb73376a2d8d63bdacea
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2015-06-05 08:30:44 +0900

    学習にいろいろ触った

--- tutorial/todo/Todo/RootViewController.swift
+++ tutorial/todo/Todo/RootViewController.swift
@@ -11,10 +11,10 @@ import UIKit
 class RootViewController: UITableViewController {
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         switch indexPath.row {
-        case 0: self.performSegueWithIdentifier("todo", sender: indexPath)
-        case 1: self.performSegueWithIdentifier("webView", sender: indexPath)
-        case 2: self.performSegueWithIdentifier("network", sender: indexPath)
-        default: break
+            case 0: self.performSegueWithIdentifier("todo", sender: indexPath)
+            case 1: self.performSegueWithIdentifier("webView", sender: indexPath)
+            case 2: self.performSegueWithIdentifier("network", sender: indexPath)
+            default: break
         }
         
 //        self.navigationController?.pushViewController(TodoViewController(), animated: true)
